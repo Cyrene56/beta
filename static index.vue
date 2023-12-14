2a32594d4b0090bb2826ba7a361eec3e67a20755
Cyrene56 <144902682+Cyrene56@users.noreply.github.com>
2023-12-14 19:30:48 +0800

    Create static index.vue

---
+++ static index.vue
@@ -0,0 +1,95 @@
+<template>
+	<view class="content">
+		<image class="banner" src="@/static/index/banner.png" mode="widthFix"></image>
+
+		<view class="list">
+			<view class="game-item" v-for="item in gameBox" @tap="goInfo(item.path)">
+				<image class="face" :src="item.faceImg" mode="widthFix"></image>
+				<view class="name">{{item.name}}</view>
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				gameBox: [
+					{
+						name: '激情飞车',
+						path: '/pages/game/car/index',
+						faceImg: '../../static/index/car.jpg'
+					},
+					{
+						name: '敬请期待...',
+						path: '',
+						faceImg: '../../static/index/more.png'
+					}
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			goInfo(path) {
+				if (path) {
+					uni.navigateTo({
+						url: path
+					})
+				} else {
+					uni.showToast({
+						icon: 'none',
+						duration: 2000,
+						title: '敬请期待...'
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		width: 100vw;
+		height: 100vh;
+		position: relative;
+
+		.banner {
+			width: 100%;
+		}
+
+		.list {
+			width: 94%;
+			padding: 3%;
+			display: flex;
+			justify-content: space-between;
+			flex-wrap: wrap;
+
+			.game-item {
+				width: 48%;
+				text-align: center;
+
+				.face {
+					width: 100%;
+				}
+
+				.name {
+					position: relative;
+					left: 0;
+					bottom: 55rpx;
+					height: 50rpx;
+					line-height: 50rpx;
+					background-color: #fff;
+					opacity: 0.6;
+					white-space: nowrap;
+					overflow: hidden;
+					text-overflow: ellipsis;
+					color: #ff5500;
+				}
+			}
+		}
+	}
+</style>
